42c6a9f93d86e4b10dff7fae68c64b090f4686d7
pevetni <carlos.rodriguez@statsperform.com>
2023-08-20 17:32:25 -0300

    section 3 - curso angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graphic1Component } from './pages/graphic1/graphic1.component';
+import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
+import { PagesComponent } from './pages/pages.component';
+
+const routes: Routes = [
+  { path: '', component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graphic1', component: Graphic1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ]
+  },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotpagefoundComponent },
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot( routes )
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
